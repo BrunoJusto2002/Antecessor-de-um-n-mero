c1144a728c5667a254ec281c96f09eafec13a001
Bruno Justo <85250651+BrunoJusto2002@users.noreply.github.com>
2022-03-24 18:00:07 -0300

    Adicionado o código

---
+++ Antecessor.js
@@ -0,0 +1,9 @@
+//Algorithm to say the predecessor
+
+//declaring the variables
+const prompt = require('utils/prompt')
+var numero = prompt("Qual valor desejado?: ",'number')
+var antecessor = numero - 1
+
+//showing the result in the terminal
+console.log("O antecessor é: " + antecessor)
